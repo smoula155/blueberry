e740c7b5dd077c9ad523d01f789687d26fb4d456
Martin Smolka <smolkam@sitd.cz>
2017-04-27 17:38:48 +0200

    fit invoice number

--- Project/app/serializers/invoice_serializer.rb
+++ Project/app/serializers/invoice_serializer.rb
@@ -1,5 +1,9 @@
 class InvoiceSerializer < ActiveModel::Serializer
-  attributes :id, :number , :price_with_vat, :price, :vat_rate, :issued_at
+  attributes :id, :number , :price_with_vat, :price, :vat_rate, :issued_at, :invoice_number
   has_one :client
   has_one :category
+
+  def invoice_number
+    self.object.number
+  end
 end
