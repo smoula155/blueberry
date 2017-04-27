7d622d076ca4d83bf082649eac2f928ef313083c
Martin Smolka <smolkam@sitd.cz>
2017-04-27 12:28:02 +0200

    Add my Gem

--- Project/config/application.rb
+++ Project/config/application.rb
@@ -19,5 +19,22 @@ module Project
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    config.after_initialize do
+      Bullet.enable = true
+      Bullet.alert = false
+      Bullet.console = true
+      Bullet.bullet_logger = false
+      Bullet.rails_logger = true
+      Bullet.add_footer = true
+      #Bullet.rollbar = true
+       #Bullet.stacktrace_includes = ['your_middleware' ]
+     #  Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware' ]
+    end
   end
 end
